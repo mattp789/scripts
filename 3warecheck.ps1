778e3de0b85748f20201eba00798b544ae409d5f
mattp789 <matthew.parker.warren@gmail.com>
2017-04-02 16:41:55 -0500

    Add files via upload
    Edited our currently standing 3ware raid controller script which sent email alerts everyday to have the logic to only send an email if a Drive had failed.

---
+++ 3warecheck.ps1
@@ -0,0 +1,29 @@
+###########Define Variables########
+
+tw_cli /c0 show all | out-file C:\3ware.txt
+tw_cli /c0 show alarms | out-file C:\3ware.txt -append
+$fromaddress = "windows-task@asc.edu"
+$toaddress = "sysadmins@asc.edu"
+$Subject = "ASAHSVHV1 3ware notification"
+$body = "3ware notification from Hyper-V server"
+$attachment = "C:\3ware.txt"
+$smtpserver = "exchange.asc.edu"
+
+####################################
+
+if((Get-Content C:\3ware.txt) -like '*SPARE     OK*'){
+exit
+}
+
+else
+{
+$message = new-object System.Net.Mail.MailMessage
+$message.From = $fromaddress
+$message.To.Add($toaddress)
+$message.IsBodyHtml = $True
+$message.Subject = $Subject
+$attach = new-object Net.Mail.Attachment($attachment)
+$message.Attachments.Add($attach)
+$message.body = $body
+$smtp = new-object Net.Mail.SmtpClient($smtpserver)
+$smtp.Send($message)}
