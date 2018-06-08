0d5a2bf4752dbb1b7f4fb3ba195abb37b64cd162
mattp789 <matthew.parker.warren@gmail.com>
2018-06-08 14:33:56 -0500

    Create windowscompression.ps1

---
+++ windowscompression.ps1
@@ -0,0 +1,61 @@
+
+$zipdir = "C:\tmp\zipdir",
+
+#Current Date and Time
+$date = (get-date -format "yyyy_MM_dd_hh_ss"),
+
+$destination = "C:\Users\matthew.parker\Desktop\backup1\zipped$date.zip",
+
+$fileextention = @("bak", "trc")
+)
+
+Add-Content $loglocation "The following files deleted:"
+
+New-Item -ItemType directory -Path $zipdir;
+
+function do-compression {
+    Param (
+        $source = " ",
+        #edit this to make it local directory filename.zip
+        $destination =  " "
+        ##################################################
+    )
+    #if(test-path $destination) {remove-item $destination}
+    add-type -Assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::CreateFromDirectory($source, $destination)
+}
+
+function Start-compression {
+
+    Param (
+
+        #Specify directory location containing files to be compressed
+
+        $directory = " "
+    )
+    foreach ($direct in $directory) {
+        compress-file -direct $direct -fileextention $fileextention
+    }
+}
+
+function compress-file {
+    Param (
+        $direct = " ",
+        $fileextention = @()
+    )
+    foreach ($dir in $direct){
+        foreach ($ext in $fileextention) {
+            $bakname = (@(get-childitem -path $direct *.$ext).name)
+            foreach ($f in $bakname) {
+            #Add-Content $loglocation "$direct\$F"
+            Move-Item $direct\$f -Destination $zipdir
+            #write-host $direct\$f
+            }
+        }
+    } 
+}
+
+
+start-compression -directory $directory
+do-compression -source $zipdir -destination $destination
+Remove-Item $zipdir -Recurse
